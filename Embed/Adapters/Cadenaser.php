6dbef103847cb609904766b5066edcab6f470813
Óscar Otero <oom@oscarotero.com>
2013-02-02 14:19:38 +0100

    Added an adapter for cadenaser.com

---
+++ Embed/Adapters/Cadenaser.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Adapter to get the embed code from cadenaser.com
+ */
+namespace Embed\Adapters;
+
+use Embed\Viewers;
+
+class Cadenaser extends Webpage implements AdapterInterface {
+	
+	public function getCode () {
+		$Dom = $this->Url->getHtmlContent();
+
+		foreach ($Dom->getElementsByTagName('div') as $Div) {
+			if ($Div->hasAttribute('class') && ($Div->getAttribute('class') === 'llevatelo')) {
+				$Input = $Div->getElementsByTagName('input');
+
+				if ($Input->length && $Input->item(0)->hasAttribute('value')) {
+					return $Input->item(0)->getAttribute('value');
+				}
+			}
+		}
+	}
+}
