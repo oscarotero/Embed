f78b41f3c3ba90a69aba35ca5d77129026009e41
Óscar Otero <oom@oscarotero.com>
2013-03-05 19:55:13 +0100

    Added Gist adapter

---
+++ Embed/Adapters/Github.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Adapter to get the embed code from codepen
+ */
+namespace Embed\Adapters;
+
+use Embed\Url;
+
+class Github extends Webpage implements AdapterInterface {
+	static public function check (Url $Url) {
+		return $Url->match(array(
+			'https://gist.github.com/*/*'
+		));
+	}
+
+	public function getCode () {
+		$url = $this->getUrl();
+
+		if (substr($url, -1) === '/') {
+			$url = substr($url, 0, -1);
+		}
+
+		return '<script src="'.$url.'.js"></script>';
+	}
+
+	public function getProviderName () {
+		return 'Gist';
+	}
+
+	public function getProviderUrl () {
+		return 'https://gist.github.com';
+	}
+}
