e561fe95f818f90ee75a1d2b04c2e0e28261cdcd
Oliver Lillie <ollie@buggedcom.co.uk>
2013-04-25 12:47:22 +0300

    merged images functionality into html provider

--- Embed/Providers/Html.php
+++ Embed/Providers/Html.php
@@ -15,20 +15,32 @@ class Html extends Provider {
 			return false;
 		}
 
+        $base_url = $Url->getUrl();
+        
+        $images = array();
+        $icons = array();
+
 		foreach ($Html->getElementsByTagName('link') as $Link) {
 			if ($Link->hasAttribute('rel') && $Link->hasAttribute('href')) {
 				$rel = trim(strtolower($Link->getAttribute('rel')));
-				$href = trim($Link->getAttribute('href'));
+				$href = $this->_getFullUrl($Link->getAttribute('href'));
 
 				if (empty($href)) {
 					continue;
 				}
 
 				switch ($rel) {
+					case 'favicon':
+					case 'favico':
 					case 'icon':
-					case 'shortcut icon':
-						$this->set('icon', $href);
-						break;
+                    case 'shortcut icon':
+                        $this->set('icon', $href);
+                        array_push($icons, $href);
+                        break;
+
+                    case 'apple-touch-icon':
+                        array_push($icons, $href);
+                        break;
 
 					case 'canonical':
 					case 'video_src':
@@ -61,10 +73,29 @@ class Html extends Provider {
 		foreach ($Html->getElementsByTagName('meta') as $Tag) {
 			if ($Tag->hasAttribute('name')) {
 				$name = strtolower($Tag->getAttribute('name'));
+                if($name === 'msapplication-tileimage'){
+                    array_push($icons, $this->_getFullUrl($base_url, $Tag->getAttribute('content')));
+                }
+                else if($name === 'twitter:image'){
+                    $img = new \stdClass();
+                    $img->width = null;
+                    $img->height = null;
+                    $img->alt = null;
+                    $img->src = $this->_getFullUrl($base_url, $Tag->getAttribute('content'));
+                    array_push($images, $img);
+                }
 			} else if ($Tag->hasAttribute('http-equiv')) {
 				$name = strtolower($Tag->getAttribute('http-equiv'));
 			} else if ($Tag->hasAttribute('property')) {
 				$name = strtolower($Tag->getAttribute('property'));
+                if ($name == 'og:image' && $Tag->hasAttribute('content')) {
+                    $img = new \stdClass();
+                    $img->width = null;
+                    $img->height = null;
+                    $img->alt = null;
+                    $img->src = $this->_getFullUrl($base_url, $Tag->getAttribute('content'));
+                    array_push($images, $img);
+                }                
 			} else {
 				continue;
 			}
@@ -73,6 +104,32 @@ class Html extends Provider {
 				$this->set($name, $Tag->getAttribute('content'));
 			}
 		}
+        
+        // images
+        foreach ($Html->getElementsByTagName('img') as $Tag) {
+            if ($Tag->hasAttribute('src')) {
+                
+                $img = new \stdClass();
+                $img->width = null;
+                $img->height = null;
+                $img->alt = null;
+                $img->src = $this->_getFullUrl($base_url, $Tag->getAttribute('src'));
+                if($Tag->hasAttribute('width') === true){
+                    $img->width = (int) $Tag->getAttribute('width');
+                }
+                if($Tag->hasAttribute('height') === true){
+                    $img->height = (int) $Tag->getAttribute('height');
+                }
+                if($Tag->hasAttribute('alt') === true){
+                    $img->alt = $Tag->getAttribute('alt');
+                }
+                
+                array_push($images, $img);
+            }
+        }
+
+        $this->set('icons', $icons);
+        $this->set('images', $images);
 	}
 
 	public function getTitle () {
@@ -115,5 +172,163 @@ class Html extends Provider {
 	public function getHeight () {
 		return $this->get('image_height') ?: $this->get('video_height');
 	}
+    
+    /**
+     * Returns an array of stdClass objects of the images found.
+     *
+     * @access public
+     * @author Oliver Lillie
+     * @return array
+     */
+    public function getImages(){
+        return $this->get('images');
+    }
+    
+    /**
+     * Returns an array of strings of the srcs of the icons found.
+     *
+     * @access public
+     * @author Oliver Lillie
+     * @return array
+     */
+    public function getIcons(){
+        return $this->get('icons');
+    }
+    
+    /**
+     * Contains the current image index.
+     *
+     * @access public
+     * @author Oliver Lillie
+     * @var integer
+     */
+    protected $_image_index = -1;
+
+    /**
+     * Returns a specific image src specified by $index, otherwise returns null.
+     * If no index is specified then the next image src is returned.
+     *
+     * @access public
+     * @author Oliver Lillie
+     * @param mixed $index Integer index of the requested image or null.
+     * @return mixed Returns a string if the given index is available otherwise
+     *  returns null.
+     */
+    public function getImageFromSet($index=null){
+        $images = $this->get('images');
+        if($index === null){
+            $index = $this->_image_index;
+            $this->_image_index += 1;
+        }
+        return isset($images[$index]) === true ? $images[$index] : null;
+    }
+    
+    /**
+     * Contains the current icon index.
+     *
+     * @access public
+     * @author Oliver Lillie
+     * @var integer
+     */
+    protected $_icon_index = -1;
+    
+    /**
+     * Returns a specific icon src specified by $index, otherwise returns null.
+     * If no index is specified then the next icon src is returned.
+     *
+     * @access public
+     * @author Oliver Lillie
+     * @param mixed $index Integer index of the requested icon or null.
+     * @return mixed Returns a string if the given index is available otherwise
+     *  returns null.
+     */
+    public function getIconFromSet($index=null){
+        $icons = $this->get('icons');
+        if($index === null){
+            $index = $this->_icon_index += 1;
+            $this->_icon_index += 1;
+        }
+        return isset($icons[$index]) === true ? $icons[$index] : null;
+    }
+    
+    
+    /**
+     * Resolves a src of an image to it's full path.
+     *
+     * @access public
+     * @author Oliver Lillie
+     * @param string $base 
+     * @param string $src 
+     * @return string
+     */
+    protected function _getFullUrl($base, $src){
+        $src = trim($src);
+        if(substr($src, 0, 2) === '//'){
+            $src = 'https:'.$src;
+        }
+        if(substr($src, 0, 4) !== 'http'){
+            $src = $this->_resolveUrl($url_string, $src);
+        }
+        
+        return $this->_resolveUrl($base, $src);
+    }
+    
+    /**
+     * Resolves urls like realpath
+     *
+     * @see http://www.php.net/manual/en/function.realpath.php#85388
+     * @access public
+     * @author Isaac Z. Schlueter i at foohack dot com
+     * @param string $base 
+     * @param string $src 
+     * @return string
+     */
+    protected function _resolveUrl($base, $href) { 
+        // href="" ==> current url. 
+        if(!$href) { 
+            return $base; 
+        } 
+
+        // href="http://..." ==> href isn't relative 
+        $rel_parsed = parse_url($href); 
+        if (array_key_exists('scheme', $rel_parsed) === true){ 
+            return $href; 
+        } 
+
+        // add an extra character so that, if it ends in a /, we don't lose the last piece. 
+        $base_parsed = parse_url($base.' '); 
+        // if it's just server.com and no path, then put a / there. 
+        if(array_key_exists('path', $base_parsed) === false){ 
+            $base_parsed = parse_url($base.'/ '); 
+        } 
+
+        // href="/ ==> throw away current path. 
+        if($href{0} === "/") { 
+            $path = $href; 
+        }
+        else{ 
+            $path = dirname($base_parsed['path']).'/'.$href; 
+        } 
+
+        // bla/./bloo ==> bla/bloo 
+        $path = preg_replace('~/\./~', '/', $path); 
+
+        // resolve /../ 
+        // loop through all the parts, popping whenever there's a .., pushing otherwise. 
+        $parts = array(); 
+        foreach (explode('/', preg_replace('~/+~', '/', $path)) as $part){
+            if($part === ".."){ 
+                array_pop($parts); 
+            }
+            else if($part!=""){ 
+                $parts[] = $part; 
+            } 
+        } 
+
+        return ( 
+            (array_key_exists('scheme', $base_parsed) === true) ? 
+                $base_parsed['scheme'] . '://' . $base_parsed['host'] : ''
+        ) . '/' . implode('/', $parts); 
+    }
 }
 ?>
