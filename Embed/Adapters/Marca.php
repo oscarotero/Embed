4e300aedb4a36d6b85f2c2ecee8c84ad497ca243
Óscar Otero <oom@oscarotero.com>
2013-10-26 15:42:55 +0200

    Added adapter to fix marca.com videos

---
+++ Embed/Adapters/Marca.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Adapter to fix the embed code from marca.com
+ */
+namespace Embed\Adapters;
+
+use Embed\Url;
+use Embed\Viewers;
+
+class Marca extends Webpage implements AdapterInterface {
+
+	static public function check (Url $Url) {
+		return $Url->match(array(
+			'http://www.marca.com/*'
+		));
+	}
+
+	public function getCode () {
+		if (($code = parent::getCode())) {
+			$errors = libxml_use_internal_errors(true);
+			$html = new \DOMDocument();
+			$result = $html->loadHtml($code);
+			libxml_use_internal_errors($errors);
+
+			if ($result) {
+				$startValue = 'http://cachevideos.marca.com/multimedia/reproductores/v2/reproductor_video.swf';
+				$startValueLength = strlen($startValue);
+
+				foreach ($html->getElementsByTagName('param') as $param) {
+					if ($param->getAttribute('name') === 'movie') {
+						$value = $param->getAttribute('value');
+
+						if (substr($value, 0, $startValueLength) === $startValue) {
+							$value = substr($value, $startValueLength);
+						}
+
+						return Viewers::videoHtml($this->getImage(), $value);
+					}
+				}
+			}
+		}
+
+		return $code;
+	}
+}
