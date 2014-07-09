8444efab5bc0b1e01da5d080f3248b395b5ddc27
oscarotero <oom@oscarotero.com>
2014-07-09 16:03:05 +0200

    Updated facebook embed code close #32

--- Embed/Adapters/Facebook.php
+++ Embed/Adapters/Facebook.php
@@ -139,11 +139,11 @@ class Facebook extends Webpage implements AdapterInterface
             return <<<EOT
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/gl_ES/all.js#xfbml=1";
-    fjs.parentNode.insertBefore(js, fjs);
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
 <div class="fb-post" data-href="{$this->url}" data-width="500"></div>
