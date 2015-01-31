6287fc7504b93cc49114fe82031b891b30a9a79b
oscarotero <oom@oscarotero.com>
2015-01-31 17:24:02 +0100

    fixed test

--- tests/GiphyTest.php
+++ tests/GiphyTest.php
@@ -8,10 +8,10 @@ class GiphyTest extends PHPUnit_Framework_TestCase
         $info = Embed\Embed::create('http://giphy.com/gifs/puppies-cute-animals-asdfghjkl-6UZFwMYqCeXi8');
 
         $this->assertEquals($info->title, 'Puppies Animated GIF');
-        $this->assertEquals($info->imageWidth, 480);
-        $this->assertEquals($info->imageHeight, 270);
         $this->assertEquals($info->type, 'video');
         $this->assertEquals($info->providerName, 'Giphy');
         $this->assertEquals($info->code, '<iframe src="https://giphy.com/embed/6UZFwMYqCeXi8?html5=true" frameborder="0" allowTransparency="true" style="border:none;overflow:hidden;width:500px;height:280px;"></iframe>');
+        $this->assertEquals($info->width, 500);
+        $this->assertEquals($info->height, 280);
     }
 }
