079af45dabfae9bbe0f73cb7f3b5caa069e90906
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-03-24 23:07:28 +0100

    Fix bad provider name on Line.do test

--- tests/LineTest.php
+++ tests/LineTest.php
@@ -10,7 +10,7 @@ class LineTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($info->code, '<iframe src="https://line.do/embed/php-evolution/vertical" frameborder="0" allowTransparency="true" style="border:none;overflow:hidden;width:640px;height:640px;"></iframe>');
         $this->assertEquals($info->width, 640);
         $this->assertEquals($info->height, 640);
-        $this->assertEquals($info->providerName, 'line');
+        $this->assertEquals($info->providerName, 'Line.do');
         $this->assertEquals($info->providerUrl, 'https://line.do');
     }
 }
