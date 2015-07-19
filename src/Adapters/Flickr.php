d154f5b14e1ba93d394a430de2fcf23ffb9f6086
oscarotero <oom@oscarotero.com>
2015-07-19 20:53:22 +0200

    recovered flickr adapter because oembed doesn't work for all urls

---
+++ src/Adapters/Flickr.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Adapter provider more information from flickr
+ */
+namespace Embed\Adapters;
+
+use Embed\Url;
+use Embed\Request;
+use Embed\Utils;
+
+class Flickr extends Webpage implements AdapterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function check(Request $request)
+    {
+        return $request->match([
+            'https://www.flickr.com/photos/*',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCode()
+    {
+        $code = parent::getCode();
+
+        if (empty($code)) {
+            $url = (new Url($this->request->getUrl()))
+                ->withAddedDirectory('player');
+
+            $code = Utils::iframe($url->getUrl(), $this->width, $this->height);
+        }
+
+        return $code;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getWidth()
+    {
+        if (!parent::getCode()) {
+            return 640;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getHeight()
+    {
+        if (!parent::getCode()) {
+            return 425;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getProviderName()
+    {
+        return 'Flickr';
+    }
+}
