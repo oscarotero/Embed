8bf5ad27d682618feb2d7ec8db510badec6b19dd
Dave Ross <dave@davidmichaelross.com>
2015-07-31 12:01:04 -0400

    WordPress.tv unit test

---
+++ tests/WordPressTest.php
@@ -0,0 +1,16 @@
+<?php
+class WordPressTest extends PHPUnit_Framework_TestCase
+{
+	public function testOne()
+	{
+		$info = Embed\Embed::create('http://wordpress.tv/2013/09/06/dave-ross-optimize-image-files-like-a-pro/');
+
+		$this->assertEquals($info->title, 'Dave Ross: Optimize Image Files Like a Pro');
+		$this->assertEquals($info->imageWidth, 400);
+		$this->assertEquals($info->imageHeight, 224);
+		$this->assertEquals($info->type, 'video');
+		$this->assertEquals($info->authorName, '@WordPressTV');
+		$this->assertEquals($info->providerName, 'WordPress.tv');
+		$this->assertEquals($info->providerUrl, 'http://wordpress.tv');
+	}
+}
