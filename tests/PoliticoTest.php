9f6166739a58624807fd51b5ca78b738776c197f
oscarotero <oom@oscarotero.com>
2015-09-17 17:34:23 +0200

    fixed Politico test

--- tests/PoliticoTest.php
+++ tests/PoliticoTest.php
@@ -6,7 +6,7 @@ class PoliticoTest extends TestCaseBase
         $this->assertEmbed(
             'http://www.politico.com/story/2013/12/presidents-barack-obama-george-w-bush-second-term-101314.html',
             [
-                'title' => 'Obama 2nd term: Echoes of Bush - POLITICO',
+                'title' => 'Obama 2nd term: Echoes of Bush',
                 'type' => 'rich',
                 'image' => 'http://s3-origin-images.politico.com/2013/12/18/131218_george_w_bush_barack_obama_ap_605.jpg',
                 'providerName' => 'POLITICO',
