713f6c184f8f637b9cc0363f0f580ed9b868a4fe
oscarotero <oom@oscarotero.com>
2015-09-20 15:08:12 +0200

    fixed url parsing in windows

--- src/Url.php
+++ src/Url.php
@@ -485,7 +485,7 @@ class Url
         $this->info['path'] = [];
 
         if (isset($parts['dirname'])) {
-            foreach (explode(DIRECTORY_SEPARATOR, $parts['dirname']) as $dir) {
+            foreach (explode('/', str_replace(DIRECTORY_SEPARATOR, '/', $parts['dirname'])) as $dir) {
                 if ($dir !== '') {
                     $this->info['path'][] = $dir;
                 }
