bcaa5eae1c795c0137e46f488142a3dd49e4908e
zyuhel <zyuhel@users.noreply.github.com>
2015-11-06 09:18:11 +0300

    Update Test according sad reality

--- tests/GiphyTest.php
+++ tests/GiphyTest.php
@@ -7,7 +7,7 @@ class GiphyTest extends TestCaseBase
         $this->assertEmbed(
             'http://giphy.com/gifs/puppies-cute-animals-asdfghjkl-6UZFwMYqCeXi8',
             [
-                'title' => 'Puppies Animated GIF',
+                'title' => 'Puppies GIF - Find & Share on GIPHY',
                 'type' => 'video',
                 'providerName' => 'Giphy',
                 'code' => '<iframe src="https://giphy.com/embed/6UZFwMYqCeXi8?html5=true" frameborder="0" allowTransparency="true" style="border:none;overflow:hidden;width:500px;height:280px;"></iframe>',
