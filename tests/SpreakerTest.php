082ffa9bce35c8e0ab5d8c5d58dadb1120a71cbf
oscarotero <oom@oscarotero.com>
2016-01-31 13:27:22 +0100

    fixed spreaker test

--- tests/SpreakerTest.php
+++ tests/SpreakerTest.php
@@ -5,11 +5,11 @@ class SpreakerTest extends TestCaseBase
     public function testOne()
     {
         $this->assertEmbed(
-            'http://www.spreaker.com/show/deseos-del-alma-poemas_1',
+            'http://www.spreaker.com/user/angelclark/angel-clark-ice-cream-tocos',
             [
-                'title' => 'Te nombro - Poemas',
+                'title' => 'Angel Clark: Ice Cream Toco\'s',
                 'type' => 'video',
-                'code' => '<iframe src="https://www.spreaker.com/embed/player/standard?autoplay=false&amp;episode_id=4535030" frameborder="0" allowTransparency="true" style="min-width:400px;border:none;overflow:hidden;width:100%;height:131px;"></iframe>',
+                'code' => '<iframe src="https://www.spreaker.com/embed/player/standard?autoplay=false&amp;episode_id=7657563" frameborder="0" allowTransparency="true" style="min-width:400px;border:none;overflow:hidden;width:100%;height:131px;"></iframe>',
                 'providerName' => 'Spreaker',
                 'providerUrl' => 'https://spreaker.com',
             ]
