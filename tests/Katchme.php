69806b323238a3a7d88639659c4c6f3b2b2097d5
oscarotero <oom@oscarotero.com>
2016-02-12 21:02:28 +0100

    added katch.me test to test #120

---
+++ tests/Katchme.php
@@ -0,0 +1,17 @@
+<?php
+
+class KatchmeTest extends TestCaseBase
+{
+    public function testOne()
+    {
+        $this->assertEmbed(
+            'https://ktch.tv/6_Lf',
+            [
+                'title' => "Backstage View 👀 of #ChineseNewYear Parade 🤓in Hong Kong #CathayCNYP",
+                'description' => 'John Ho',
+                'type' => 'link',
+                'providerName' => 'Katch',
+            ]
+        );
+    }
+}
