0d8ae47afab283192315f63f7ecff04309f9f169
oscarotero <oom@oscarotero.com>
2016-04-14 18:58:42 +0200

    fixed imageInfoInterface

--- src/ImageInfo/ImageInfoInterface.php
+++ src/ImageInfo/ImageInfoInterface.php
@@ -8,16 +8,6 @@ namespace Embed\ImageInfo;
 interface ImageInfoInterface
 {
     /**
-     * Get the info of multiple images at once.
-     *
-     * @param array      $urls
-     * @param null|array $config
-     *
-     * @return array
-     */
-    public static function getImagesInfo(array $urls, array $config = null);
-
-    /**
      * Returns the headers.
      * 
      * @return array
