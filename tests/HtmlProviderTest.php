f5d61729ddf1922e858f602367fc58a27be83269
oscarotero <oom@oscarotero.com>
2016-05-21 12:55:26 +0200

    added test for url patterns

--- tests/HtmlProviderTest.php
+++ tests/HtmlProviderTest.php
@@ -33,4 +33,23 @@ class HtmlProviderTest extends TestCaseBase
             ]
         );
     }
+
+    public function testAllowSomeExternalImages()
+    {
+        $this->assertEmbed(
+            'http://www.brothers-brick.com/2016/04/06/stunning-lego-darth-vader-mask-cleverly-hides-scenes-from-star-wars/',
+            [
+                'image' => 'http://i0.wp.com/farm2.staticflickr.com/1458/25975786430_c69776287e_z.jpg?resize=625%2C495&ssl=1',
+            ], [
+                'providers' => [
+                    'html' => [
+                        'externalImages' => [
+                            '*.staticflickr.com/*',
+                            '*.flickr.com/*',
+                        ],
+                    ],
+                ],
+            ]
+        );
+    }
 }
