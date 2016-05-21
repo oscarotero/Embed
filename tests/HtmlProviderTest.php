08a1b96cb05ab0576a8c8f50be35c43cb9dce473
oscarotero <oom@oscarotero.com>
2016-05-21 12:46:48 +0200

    added test to html provider

---
+++ tests/HtmlProviderTest.php
@@ -0,0 +1,36 @@
+<?php
+
+class HtmlProviderTest extends TestCaseBase
+{
+    public function testAllowExternalImages()
+    {
+        $this->assertEmbed(
+            'http://www.brothers-brick.com/2016/04/06/stunning-lego-darth-vader-mask-cleverly-hides-scenes-from-star-wars/',
+            [
+                'image' => 'http://i0.wp.com/farm2.staticflickr.com/1458/25975786430_c69776287e_z.jpg?resize=625%2C495&ssl=1',
+            ], [
+                'providers' => [
+                    'html' => [
+                        'externalImages' => true,
+                    ],
+                ],
+            ]
+        );
+    }
+
+    public function testNotAllowExternalImages()
+    {
+        $this->assertEmbed(
+            'http://www.brothers-brick.com/2016/04/06/stunning-lego-darth-vader-mask-cleverly-hides-scenes-from-star-wars/',
+            [
+                'image' => '',
+            ], [
+                'providers' => [
+                    'html' => [
+                        'externalImages' => false,
+                    ],
+                ],
+            ]
+        );
+    }
+}
