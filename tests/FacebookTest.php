7dc18c9dc0e713dafc25ea4d0d3c8dc0126471ec
oscarotero <oom@oscarotero.com>
2016-05-27 16:39:28 +0200

    tested posts from facebook pages #147

--- tests/FacebookTest.php
+++ tests/FacebookTest.php
@@ -2,12 +2,25 @@
 
 class FacebookTest extends TestCaseBase
 {
-    public function testOne()
+    public function testPermalink()
     {
         $this->assertEmbed(
             'https://www.facebook.co/permalink.php?story_fbid=827163017327807&id=149460691764713',
             [
                 'title' => 'Aquí vos deixamos unhas imaxes da nosa... - DAG, Asociación Galega de Deseñadores | Facebook',
+                'width' => 552,
+                'code' => true,
+                'type' => 'rich',
+            ]
+        );
+    }
+
+    public function testCompanyPage()
+    {
+        $this->assertEmbed(
+            'https://www.facebook.com/TheIndependentOnline/posts/10153659888571636',
+            [
+                'title' => 'The Independent - Best. News. Ever. | Facebook',
                 'width' => 552,
                 'code' => true,
                 'type' => 'rich',
