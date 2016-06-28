8fcbaee2b60d60bf09e78531c3e158e9f0176765
oscarotero <oom@oscarotero.com>
2016-06-28 13:46:41 +0200

    added support for cartodb.com

---
+++ src/Adapters/Cartodb.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Embed\Adapters;
+
+use Embed\Request;
+use Embed\Utils;
+
+/**
+ * Adapter to get the embed code from cartodb.
+ */
+class Cartodb extends Webpage implements AdapterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function check(Request $request)
+    {
+        return $request->isValid() && $request->match([
+            'https://*.cartodb.com/viz/*/public_map',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCode()
+    {
+        $this->width = null;
+        $this->height = 520;
+
+        $url = $this->request->createUrl()->withDirectoryPosition(2, 'embed_map')->getUrl();
+        
+        return Utils::iframe($url, '100%', $this->height);
+    }
+}
---
+++ tests/CartodbTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class CartodbTest extends TestCaseBase
+{
+    public function testOne()
+    {
+        $this->assertEmbed(
+            'https://porcentua26j.cartodb.com/viz/ab0f51e6-3c16-11e6-b12e-0e5db1731f59',
+            [
+                'title' => 'Resultados 26J / 20Dj',
+                'height' => 520,
+                'type' => 'rich',
+                'code' => '<iframe src="https://porcentua26j.cartodb.com/viz/ab0f51e6-3c16-11e6-b12e-0e5db1731f59/embed_map" frameborder="0" allowTransparency="true" style="border:none;overflow:hidden;width:100%;height:520px;"></iframe>',
+                'providerName' => 'CartoDB',
+            ]
+        );
+    }
+}
