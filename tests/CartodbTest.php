ac7043e4da3994b1b2fea631b2bdd48ceaead7ad
oscarotero <oom@oscarotero.com>
2016-07-09 14:14:22 +0200

    fixed cartodb test

--- tests/CartodbTest.php
+++ tests/CartodbTest.php
@@ -10,8 +10,8 @@ class CartodbTest extends TestCaseBase
                 'title' => 'Resultados 26J / 20Dj',
                 'height' => 520,
                 'type' => 'rich',
-                'code' => '<iframe src="https://porcentua26j.cartodb.com/viz/ab0f51e6-3c16-11e6-b12e-0e5db1731f59/embed_map" frameborder="0" allowTransparency="true" style="border:none;overflow:hidden;width:100%;height:520px;"></iframe>',
-                'providerName' => 'CartoDB',
+                'code' => '<iframe src="https://porcentua26j.carto.com/viz/ab0f51e6-3c16-11e6-b12e-0e5db1731f59/embed_map" frameborder="0" allowTransparency="true" style="border:none;overflow:hidden;width:100%;height:520px;"></iframe>',
+                'providerName' => 'CARTO',
             ]
         );
     }
