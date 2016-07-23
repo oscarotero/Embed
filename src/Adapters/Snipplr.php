471b7d87ca05ae244503d9f1c153ff299e74dc3e
oscarotero <oom@oscarotero.com>
2016-07-23 12:55:14 +0200

    Added snipplr.com support #156

---
+++ src/Adapters/Snipplr.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Embed\Adapters;
+
+use Embed\Request;
+use Embed\Utils;
+
+/**
+ * Adapter to generate embed code from snipplr.com.
+ */
+class Snipplr extends Webpage implements AdapterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function check(Request $request)
+    {
+        return $request->isValid() && $request->match([
+            'https?://snipplr.com/view/*',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCode()
+    {
+        $this->width = null;
+        $this->height = null;
+
+        $id = $this->request->getDirectoryPosition(1);
+
+        return <<<CODE
+<div id="snipplr_embed_{$id}" class="snipplr_embed"><a target_"blank" href="http://snipplr.com/view/{$id}">View this snippet</a> on Snipplr</div><script type="text/javascript" src="http://snipplr.com/js/embed.js"></script><script type="text/javascript" src="http://snipplr.com/json/{$id}"></script>
+CODE;
+    }
+}
---
+++ tests/SnipplrTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class SnipplrTest extends TestCaseBase
+{
+    public function testOne()
+    {
+        $this->assertEmbed(
+            'http://snipplr.com/view/72914/better-html-5-basic-starter-template',
+            [
+                'title' => 'Better HTML 5 basic starter template - HTML - Snipplr Social Snippet Repository',
+                'type' => 'rich',
+                'providerName' => 'snipplr',
+                'providerUrl' => 'http://snipplr.com',
+                'code' => '<div id="snipplr_embed_72914" class="snipplr_embed"><a target_"blank" href="http://snipplr.com/view/72914">View this snippet</a> on Snipplr</div><script type="text/javascript" src="http://snipplr.com/js/embed.js"></script><script type="text/javascript" src="http://snipplr.com/json/72914"></script>'
+            ]
+        );
+    }
+}
