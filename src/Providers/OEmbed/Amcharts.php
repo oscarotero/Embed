17882c06471242895cbdfd724b6db03a3a8d42bd
Janez Urevc <janez@janezurevc.name>
2016-09-01 11:36:27 +0200

    Provide AmCharts support.

---
+++ src/Providers/OEmbed/Amcharts.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Embed\Providers\OEmbed;
+
+use Embed\Url;
+
+class Amcharts extends OEmbedImplementation
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getEndPoint(Url $url)
+    {
+        return 'https://live.amcharts.com/oembed';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getPatterns()
+    {
+        return ['https?://live.amcharts.com/*'];
+    }
+}
