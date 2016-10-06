ed3cf654400a2aa917b832fb6644b668243202a7
Dan Braghis <dan.braghis@torchbox.com>
2016-10-06 23:13:50 +0100

    Fix tests
    TED embed URL has changed.
    The test tweets author name is capitalised.

--- tests/TedTest.php
+++ tests/TedTest.php
@@ -11,7 +11,7 @@ class TedTest extends TestCaseBase
                 'description' => "Wolves were once native to the US' Yellowstone National Park -- until hunting wiped them out. But when, in 1995, the wolves began to come back (thanks to an aggressive management program), something interesting happened: the rest of the park began to find a new, more healthful balance. In a bold thought experiment, George Monbiot imagines a wilder world in which humans work to restore the complex, lost natural food chains that once surrounded us.",
                 'imageWidth' => 240,
                 'imageHeight' => 180,
-                'code' => '<iframe src="https://embed-ssl.ted.com/talks/george_monbiot_for_more_wonder_rewild_the_world.html" width="560" height="315" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>',
+                'code' => '<iframe src="https://embed.ted.com/talks/george_monbiot_for_more_wonder_rewild_the_world" width="560" height="315" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>',
                 'type' => 'video',
                 'authorName' => 'George Monbiot',
                 'authorUrl' => 'http://www.ted.com/speakers/george_monbiot',
