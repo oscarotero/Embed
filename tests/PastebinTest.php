c3e2a183c78a78641f77c87c14ef2c9a4e924a98
Alex Bouma <me@alexbouma.me>
2017-07-08 16:26:38 +0200

    Update Pastebin adapter

--- tests/PastebinTest.php
+++ tests/PastebinTest.php
@@ -7,7 +7,7 @@ class PastebinTest extends AbstractTestCase
     public function testOne()
     {
         $this->assertEmbed(
-            'http://pastebin.com/d4biUtRm',
+            'https://pastebin.com/d4biUtRm',
             [
                 'title' => '[Bash] Pushing new git submodule to Heroku - Pastebin.com',
                 'type' => 'rich',
