cca983f64675863e7e99252097fdc1cd2010277a
oscarotero <oom@oscarotero.com>
2017-07-08 11:39:27 +0200

    meetup test fix

--- tests/MeetUpTest.php
+++ tests/MeetUpTest.php
@@ -10,8 +10,8 @@ class MeetUpTest extends AbstractTestCase
             'https://www.meetup.com/SetPay-Talks/events/219650150/',
             [
                 'title' => 'Docker, from zero to hero with Carlos Sánchez',
-                'description' => "Inauguramos las charlas de SetPay con uno de los temas más de moda en 2014, Docker.\n\n Docker es una herramienta para que desarrolladores y administradores de sistemas construyan, desplieguen y ejecute",
-                'imageWidth' => 1260,
+                'description' => "Inauguramos las charlas de SetPay con uno de los temas más de moda en 2014, Docker.\n\n Docker es una herramienta para que desarrolladores y administradores de sistemas construyan, desplieguen y ejecuten",
+                'imageWidth' => 2400,
                 'imageHeight' => 1260,
                 'type' => 'rich',
             ]
