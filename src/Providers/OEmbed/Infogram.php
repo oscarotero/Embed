ebe107eacfc111d99271c0b89771fd8d9ce0af0b
Matt Robison <bainem@gmail.com>
2018-01-17 10:45:32 -0500

    Add support for Infogram oembed.

---
+++ src/Providers/OEmbed/Infogram.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Embed\Providers\OEmbed;
+
+use Embed\Http\Url;
+
+class Infogram extends EndPoint implements EndPointInterface
+{
+    protected static $pattern = [
+        'infogr.am/*',
+        'www.infogr.am/*',
+    ];
+    protected static $endPoint = 'https://infogr.am/oembed';
+
+        /**
+     * {@inheritdoc}
+     */
+    public function getEndPoint()
+    {
+        $url = $this->response->getUrl()->withScheme('https');
+
+        return Url::create(static::$endPoint)
+                ->withQueryParameters([
+                    'url' => (string) $url,
+                ]);
+    }
+}
