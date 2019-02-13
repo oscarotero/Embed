5a1677f19274b76ae93cfffa7626b935b7c37e51
Oscar Otero <oom@oscarotero.com>
2019-02-13 10:06:53 +0100

    fixed polldaddy

--- tests/PolldaddyTest.php
+++ tests/PolldaddyTest.php
@@ -11,9 +11,9 @@ class PolldaddyTest extends AbstractTestCase
             [
                 'title' => 'Which design do you prefer?',
                 'description' => 'Poll Answers: Option #1, Option #2,',
-                'code' => '<script type="text/javascript" charset="utf-8" src="https://secure.polldaddy.com/p/7012505.js"></script><noscript><a href="https://polldaddy.com/poll/7012505/">Which design do you prefer?</a></noscript>',
+                'code' => '<script type="text/javascript" charset="utf-8" src="https://secure.polldaddy.com/p/7012505.js"></script><noscript><a href="https://poll.fm/7012505">Which design do you prefer?</a></noscript>',
                 'type' => 'rich',
-                'providerName' => 'Polldaddy',
+                'providerName' => 'Crowdsignal',
             ]
         );
     }
