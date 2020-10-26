179415b8d4cfd74602c00facd24534ec917e92a8
Floris Derks <floris.derks@npo.nl>
2020-10-26 23:59:02 +0100

    Fix Instagram/Facebook oEmbed calls

--- src/Providers/OEmbed/Facebook.php
+++ src/Providers/OEmbed/Facebook.php
@@ -2,11 +2,35 @@
 
 namespace Embed\Providers\OEmbed;
 
+use Embed\Adapters\Adapter;
+use Embed\Http\Response;
 use Embed\Http\Url;
 
 class Facebook extends EndPoint implements EndPointInterface
 {
     protected static $pattern = 'www.facebook.com/*';
+    protected $key;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(Adapter $adapter)
+    {
+        $key = $adapter->getConfig('facebook[key]');
+
+        if (!empty($key)) {
+            return new static($adapter->getResponse(), $key);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function __construct(Response $response, $key = null)
+    {
+        $this->response = $response;
+        $this->key = $key;
+    }
 
     /**
      * {@inheritdoc}
@@ -14,14 +38,15 @@ class Facebook extends EndPoint implements EndPointInterface
     public function getEndPoint()
     {
         if ($this->getUrl()->match(['*/videos/*', '/video.php'])) {
-            $endPoint = Url::create('https://www.facebook.com/plugins/video/oembed.json');
+            $endPoint = Url::create('https://graph.facebook.com/v8.0/oembed_video');
         } else {
-            $endPoint = Url::create('https://www.facebook.com/plugins/post/oembed.json');
+            $endPoint = Url::create('https://graph.facebook.com/v8.0/oembed_post');
         }
 
         return $endPoint->withQueryParameters([
             'url' => (string) $this->getUrl(),
             'format' => 'json',
+            'access_token' => $this->key,
         ]);
     }
 }
--- src/Providers/OEmbed/Instagram.php
+++ src/Providers/OEmbed/Instagram.php
@@ -2,6 +2,8 @@
 
 namespace Embed\Providers\OEmbed;
 
+use Embed\Adapters\Adapter;
+use Embed\Http\Response;
 use Embed\Http\Url;
 
 class Instagram extends EndPoint implements EndPointInterface
@@ -11,7 +13,29 @@ class Instagram extends EndPoint implements EndPointInterface
         'www.instagram.com/p/*',
         'instagr.am/p/*',
     ];
-    protected static $endPoint = 'https://api.instagram.com/oembed';
+    protected static $endPoint = 'https://graph.facebook.com/v8.0/instagram_oembed';
+    protected $key;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(Adapter $adapter)
+    {
+        $key = $adapter->getConfig('facebook[key]');
+
+        if (!empty($key)) {
+            return new static($adapter->getResponse(), $key);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function __construct(Response $response, $key = null)
+    {
+        $this->response = $response;
+        $this->key = $key;
+    }
 
     /**
      * {@inheritdoc}
@@ -24,6 +48,7 @@ class Instagram extends EndPoint implements EndPointInterface
                 ->withQueryParameters([
                     'url' => (string) $url,
                     'format' => 'json',
+                    'access_token' => $this->key,
                 ]);
     }
 }
