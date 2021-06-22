120bfa3e604c366be44c7a25f6a9e1d96e41d37f
Oscar Otero <oom@oscarotero.com>
2021-06-22 16:54:23 +0200

    return ->image for image link #452

--- src/Detectors/Image.php
+++ src/Detectors/Image.php
@@ -19,6 +19,20 @@ class Image extends Detector
             ?: $oembed->url('thumbnail_url')
             ?: $metas->url('og:image', 'og:image:url', 'og:image:secure_url', 'twitter:image', 'twitter:image:src', 'lp:image')
             ?: $document->link('image_src')
-            ?: $ld->url('image.url');
+            ?: $ld->url('image.url')
+            ?: $this->detectFromContentType();
+    }
+
+    private function detectFromContentType()
+    {
+        if (!$this->extractor->getResponse()->hasHeader('content-type')) {
+            return null;
+        }
+
+        $contentType = $this->extractor->getResponse()->getHeader('content-type')[0];
+
+        if (strpos($contentType, 'image/') === 0) {
+            return $this->extractor->getUri();
+        }
     }
 }
